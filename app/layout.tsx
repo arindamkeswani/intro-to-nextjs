29d79f94ed68e678af3d7f681c280cf36d297f3b
Arindam Keswani <arindamkeswani@gmail.com>
2024-12-15 23:45:24 +0530

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Introduction",
+  description: "Basic NextJS App for Arindam",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div className='p-4 border-b'>
+          Introduction to Next.js
+        </div>
+        {children}
+      </body>
+    </html>
+  );
+}
